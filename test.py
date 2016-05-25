1600d17d6264c2147ac677a753ed2858d947c7f6
milescroxford <croxford.miles@gmail.com>
2016-05-25 16:40:24 +0100

    typo

--- test.py
+++ test.py
@@ -7,8 +7,6 @@ class Vehicles:
         self.vehicles = self.read_json('vehicles.json')
         self.vehicle_groups = self.read_json('vehicle_groups.json')
 
-        pprint(self.vehicle_groups)
-
     def read_json(self, file):
         with open(file) as data_file:
             return json.load(data_file)
